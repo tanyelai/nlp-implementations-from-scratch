905df06a6539f3c04c125ebb7b2ccda85e798db6
Toygar Tanyel <44132720+Toygarr@users.noreply.github.com>
2021-09-08 09:28:40 +0300

    Add files via upload

---
+++ Byte Pair Encoding/bpe-main.py
@@ -0,0 +1,67 @@
+import re, collections, wget
+
+def get_vocab(filename):
+    vocab = collections.defaultdict(int)
+    with open(filename, 'r', encoding='utf-8') as fhand:
+        for line in fhand:
+            words = line.strip().split()
+            for word in words:
+                vocab[' '.join(list(word)) + ' </w>'] += 1
+    return vocab
+
+def get_stats(vocab):
+    pairs = collections.defaultdict(int)
+    for word, freq in vocab.items():
+        symbols = word.split()
+        for i in range(len(symbols)-1):
+            pairs[symbols[i],symbols[i+1]] += freq
+    return pairs
+
+def merge_vocab(pair, v_in):
+    v_out = {}
+    bigram = re.escape(' '.join(pair))
+    p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
+    for word in v_in:
+        w_out = p.sub(''.join(pair), word)
+        v_out[w_out] = v_in[word]
+    return v_out
+
+def get_tokens(vocab):
+    tokens = collections.defaultdict(int)
+    for word, freq in vocab.items():
+        word_tokens = word.split()
+        for token in word_tokens:
+            tokens[token] += freq
+    return tokens
+
+vocab = get_vocab('vocab.txt') 
+
+#########################################
+
+    
+'''
+There is free book from Gutenberg
+pg16457.txt is real-life example if you want to try.
+'''
+# download = wget.download('http://www.gutenberg.org/cache/epub/16457/pg16457.txt')  
+# vocab = get_vocab(download) #to make our real word set avaliable to use
+
+# if you cannot use wget just install from pip:
+# https://pypi.org/project/wget/
+
+##########################################
+
+num_merges = 100
+
+for i in range(num_merges):
+    pairs = get_stats(vocab)
+    if not pairs:
+        break
+    best = max(pairs, key=pairs.get)
+    vocab = merge_vocab(best, vocab)
+    print('Iter: {}'.format(i))
+    print('Best pair: {}'.format(best))
+    tokens = get_tokens(vocab)
+    print('Tokens: {}'.format(tokens))
+    print('Number of tokens: {}'.format(len(tokens)))
+    print('___________________________________________')
